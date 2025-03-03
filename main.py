64fe826cfa65009109656943b36298451b092de3
reinisciemins <144588742+reinisciemins@users.noreply.github.com>
2025-03-03 15:47:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-
+#Klase, kas atbilst vienai virsotnei spēles kokā
+class Virsotne:
+    
+    #Klases konstruktors, kas izveido virsotnes eksemplāru
+    #Katrā virsotnes eksmeplārā glabājas virsotnes unikāls identifikators (id), skaitliskā virkne (virkne)
+    #pirmā spēlētāja punkti (p1), otrā spēlētāja punkti(p2), un virsotnes atrašanās līmeņa numurs
+    #Glabātie dati tiek padoti kā konstruktora argumenti
+    def __init__(self, id, virkne, p1, p2, limenis):
+        self.id=id
+        self.virkne=virkne
+        self.p1=p1
+        self.p2=p2
+        self.limenis=limenis
+               
+#Klase, kas atbilst spēles kokam        
+class Speles_koks:
+    
+    #Klases konstruktors, kas izveido spēles koka eksemplāru
+    #Spēles koka eksemplārs ietver sevī virsotņu kopu, kas tiek veidota kā Python saraksts un
+    #loku kopu, kas tiek veidota kā Python vārdnīca (dictionary)
+    #Gan virsotņu kopa, gan loku kopa sākotnējie ir tukšas
+    #Virsotņu kopā glabāsies virsotnes viena aiz otras
+    #Loku kopā glabāsies virsotnes unikāls identifikators kā vārdnīcas atslēga (key) un
+    #ar konkrētu virsotni citu saistītu virsotņu unikālie identifikatori kā vērtības (values)
+    def __init__(self):
+        self.virsotnu_kopa=[]
+        self.loku_kopa=dict()
+    
+    #Klases Speles_koks metode, kas pievieno spēles kokam jaunu virsotni, kuru saņem kā argumentu
+    def pievienot_virsotni(self, Virsotne):
+        self.virsotnu_kopa.append(Virsotne)
+        
+    #Klases Speles_koks metode, kura papildina loku kopu, saņemot kā argumentus
+    #virsotnes identifikatoru, no kuras loks iziet, un virsotnes identifikatoru, kurā loks ieiet
+    def pievienot_loku(self, sakumvirsotne_id, beiguvirsotne_id):
+        self.loku_kopa[sakumvirsotne_id]=self.loku_kopa.get(sakumvirsotne_id,[])+[beiguvirsotne_id]
